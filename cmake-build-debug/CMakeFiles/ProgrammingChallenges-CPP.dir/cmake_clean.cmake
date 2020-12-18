1b999cc30e915a78db295ac1974809b1fbc5617c
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2020-12-18 17:15:57 +0300

    Initial Commit.

---
+++ cmake-build-debug/CMakeFiles/ProgrammingChallenges-CPP.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ProgrammingChallenges-CPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ProgrammingChallenges_CPP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProgrammingChallenges_CPP.dir/GettingStarted/G1/The3nPlus1.cpp.o"
+  "ProgrammingChallenges_CPP"
+  "ProgrammingChallenges_CPP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProgrammingChallenges_CPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
